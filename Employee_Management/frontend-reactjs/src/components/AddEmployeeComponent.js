8cc45d497dffe753560dbb573b17d2688ea31641
Saket Maniyar <saketmaniyar@gmail.com>
2023-09-09 18:54:06 +0530

    AddEmployeeComponent

---
+++ Employee_Management/frontend-reactjs/src/components/AddEmployeeComponent.js
@@ -0,0 +1,87 @@
+import React, {useState, useEffect} from 'react'
+import {useParams } from 'react-router-dom';
+import EmployeeService from '../services/EmployeeService'
+
+const AddEmployeeComponent = () => {
+
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [emailId, setEmailId] = useState('')
+
+    const {id} = useParams();
+
+
+    useEffect(() => {
+
+        EmployeeService.getEmployeeById(id).then((response) =>{
+            setFirstName(response.data.firstName)
+            setLastName(response.data.lastName)
+            setEmailId(response.data.emailId)
+        }).catch(error => {
+            console.log(error)
+        })
+    }, [id])
+
+
+    return (
+        <div>
+           <br /><br />
+           <div className = "container">
+                <div className = "row">
+                    <div className = "card col-md-6 offset-md-3 offset-md-3">
+                       {
+                           title()
+                       }
+                        <div className = "card-body">
+                            <form>
+                                <div className = "form-group mb-2">
+                                    <label className = "form-label"> First Name :</label>
+                                    <input
+                                        type = "text"
+                                        placeholder = "Enter first name"
+                                        name = "firstName"
+                                        className = "form-control"
+                                        value = {firstName}
+                                        onChange = {(e) => setFirstName(e.target.value)}
+                                    >
+                                    </input>
+                                </div>
+
+                                <div className = "form-group mb-2">
+                                    <label className = "form-label"> Last Name :</label>
+                                    <input
+                                        type = "text"
+                                        placeholder = "Enter last name"
+                                        name = "lastName"
+                                        className = "form-control"
+                                        value = {lastName}
+                                        onChange = {(e) => setLastName(e.target.value)}
+                                    >
+                                    </input>
+                                </div>
+
+                                <div className = "form-group mb-2">
+                                    <label className = "form-label"> Email Id :</label>
+                                    <input
+                                        type = "email"
+                                        placeholder = "Enter email Id"
+                                        name = "emailId"
+                                        className = "form-control"
+                                        value = {emailId}
+                                        onChange = {(e) => setEmailId(e.target.value)}
+                                    >
+                                    </input>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+
+           </div>
+
+        </div>
+    )
+}
+
+export default AddEmployeeComponent
