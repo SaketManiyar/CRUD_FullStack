012dbf6b64a2c9cb052333e230691f73e1d13dba
Saket Maniyar <saketmaniyar@gmail.com>
2023-09-09 15:04:48 +0530

    EmployeeController

---
+++ Employee_Management/backend-springboot/src/main/java/net/javacode/springboot/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package net.javacode.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import net.javacode.springboot.exception.ResourceNotFoundException;
+import net.javacode.springboot.model.Employee;
+import net.javacode.springboot.repository.EmployeeRepository;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/v1/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    // build create employee REST API
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+}
